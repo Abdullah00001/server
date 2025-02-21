ab648d981494c5b9de770e625502b3793abe108e
Abdullah Bin Omar Chowdhury <abdullahbinomarchowdhury@gmail.com>
2025-02-21 19:19:17 +0600

    intital commit

---
+++ notice/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+# Create your models here.
+class AllNotice(models.Model):
+    allNoticeTitle = models.CharField(max_length=500, verbose_name='নোটিশ এর নাম')
+    allNoticeCrated = models.DateTimeField(auto_now_add=True)
+    allNoticeUpdate = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.allNoticeTitle}'
+    
+    class Meta:
+        verbose_name_plural = 'সব নোটিশ'
+
+class RecentNotice(models.Model):
+    recentNoticeTitle = models.CharField(max_length=500, verbose_name='নোটিশ এর নাম')
+    recentNoticeDescription = models.TextField(verbose_name='নোটিশ এর বিস্তারিত')
+    recentNoticeCrated = models.DateTimeField(auto_now_add=True)
+    recentNoticeUpdate = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f'{self.recentNoticeTitle} - {self.recentNoticeDescription}'
+    
+    class Meta:
+        verbose_name_plural = 'রিসেন্ট অদ্য কয়েকটি নোটিশ'
